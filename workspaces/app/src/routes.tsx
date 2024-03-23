dc7246682b85087bc3dd3d9ac8ea9d3f2157cbbb
asa1984 <satoasa9913@gmail.com>
2024-03-24 02:44:51 +0900

    lazy import

--- workspaces/app/src/routes.tsx
+++ workspaces/app/src/routes.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { Suspense, lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { styled } from 'styled-components';
 
@@ -8,11 +8,12 @@ import { Text } from './foundation/components/Text';
 import { ActionLayout } from './foundation/layouts/ActionLayout';
 import { CommonLayout } from './foundation/layouts/CommonLayout';
 import { Color, Space, Typography } from './foundation/styles/variables';
-import { AuthorDetailPage } from './pages/AuthorDetailPage';
-import { BookDetailPage } from './pages/BookDetailPage';
-import { EpisodeDetailPage } from './pages/EpisodeDetailPage';
-import { SearchPage } from './pages/SearchPage';
-import { TopPage } from './pages/TopPage';
+
+const AuthorDetailPage = lazy(() => import('./pages/AuthorDetailPage'));
+const BookDetailPage = lazy(() => import('./pages/BookDetailPage'));
+const EpisodeDetailPage = lazy(() => import('./pages/EpisodeDetailPage'));
+const SearchPage = lazy(() => import('./pages/SearchPage'));
+const TopPage = lazy(() => import('./pages/TopPage'));
 
 const _BackToTopButton = styled(Link)`
   display: flex;
@@ -25,30 +26,32 @@ const _BackToTopButton = styled(Link)`
 
 export const Router: React.FC = () => {
   return (
-    <Routes>
-      <Route element={<CommonLayout />} path={'/'}>
-        <Route element={<TopPage />} path={''} />
-      </Route>
-      <Route
-        element={
-          <ActionLayout
-            leftContent={
-              <_BackToTopButton href={'/'}>
-                <ArrowBack style={{ color: Color.MONO_100, height: 32, width: 32 }} />
-                <Text color={Color.MONO_100} typography={Typography.NORMAL16} weight="bold">
-                  トップへ戻る
-                </Text>
-              </_BackToTopButton>
-            }
-          />
-        }
-        path={'/'}
-      >
-        <Route element={<BookDetailPage />} path={'books/:bookId'} />
-        <Route element={<EpisodeDetailPage />} path={'books/:bookId/episodes/:episodeId'} />
-        <Route element={<AuthorDetailPage />} path={'authors/:authorId'} />
-        <Route element={<SearchPage />} path={'search'} />
-      </Route>
-    </Routes>
+    <Suspense>
+      <Routes>
+        <Route element={<CommonLayout />} path={'/'}>
+          <Route element={<TopPage />} path={''} />
+        </Route>
+        <Route
+          element={
+            <ActionLayout
+              leftContent={
+                <_BackToTopButton href={'/'}>
+                  <ArrowBack style={{ color: Color.MONO_100, height: 32, width: 32 }} />
+                  <Text color={Color.MONO_100} typography={Typography.NORMAL16} weight="bold">
+                    トップへ戻る
+                  </Text>
+                </_BackToTopButton>
+              }
+            />
+          }
+          path={'/'}
+        >
+          <Route element={<BookDetailPage />} path={'books/:bookId'} />
+          <Route element={<EpisodeDetailPage />} path={'books/:bookId/episodes/:episodeId'} />
+          <Route element={<AuthorDetailPage />} path={'authors/:authorId'} />
+          <Route element={<SearchPage />} path={'search'} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 };
