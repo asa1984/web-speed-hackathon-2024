1c51dafa0bc760ee0d7531b81a080fc9fb69b3ab
asa1984 <satoasa9913@gmail.com>
2024-03-24 10:50:43 +0900

    jxl変換SWの変換先をbmpからpngに変更

--- workspaces/vite/src/serviceworker/transformJpegXLToBmp.ts
+++ workspaces/vite/src/serviceworker/transformJpegXLToBmp.ts
@@ -12,12 +12,12 @@ export async function transformJpegXLToBmp(response: Response): Promise<Response
   });
 
   const imageData = decode(await response.arrayBuffer())!;
-  const bmpBinary = await new Jimp(imageData).getBufferAsync(Jimp.MIME_BMP);
+  const pngBinary = await new Jimp(imageData).getBufferAsync(Jimp.MIME_PNG);
 
-  return new Response(bmpBinary, {
+  return new Response(pngBinary, {
     headers: {
       'Cache-Control': 'no-store',
-      'Content-Type': 'image/bmp',
+      'Content-Type': 'image/png',
     },
   });
 }
