0f45a8b259a1e510467656314ed67c5431d0a841
asa1984 <satoasa9913@gmail.com>
2024-03-24 11:14:06 +0900

    ビルドオプションの最適化

--- workspaces/vite/vite.config.ts
+++ workspaces/vite/vite.config.ts
@@ -1,8 +1,22 @@
 import react from '@vitejs/plugin-react-swc';
+import { visualizer } from 'rollup-plugin-visualizer';
 import { defineConfig } from 'vite';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: 'chrome120',
+    rollupOptions: {
+      plugins: [
+        process.env.ANALYZE &&
+          visualizer({
+            filename: 'dist/stats.html',
+            gzipSize: true,
+            brotliSize: true,
+          }),
+      ],
+    },
+  },
   plugins: [react(), nodePolyfills()],
 });
